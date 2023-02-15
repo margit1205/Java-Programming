e706d994d1a50d83a521b22aca5f21b698ea7b8f
MD AFTAB RAJA <124656060+margit1205@users.noreply.github.com>
2023-02-15 11:12:40 +0530

    Delete VotingRegistraion directory

--- VotingRegistraion/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.votingregistraion {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.votingregistraion to javafx.fxml;
-    exports com.example.votingregistraion;
-}
